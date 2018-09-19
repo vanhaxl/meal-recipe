4f8de9412b4a3cfa6bacf0290e036086a6deff52
Van Ha Nguyen <vanhanguyen@Vans-MacBook-Pro.local>
2018-09-19 09:51:44 -0500

    test

--- src/main/java/altrimetrik/playground/controller/AddMoreClass.java
+++ src/main/java/altrimetrik/playground/controller/AddMoreClass.java
@@ -2,14 +2,10 @@ package altrimetrik.playground.controller;
 
 public class AddMoreClass {
     //test once more time
-<<<<<<< HEAD
     //test more
     //test more
     public String crypto(String input){
         return "encrypted String";
-=======
-    public void test(){
-        System.out.println("hi");
->>>>>>> branch1
+
     }
 }
