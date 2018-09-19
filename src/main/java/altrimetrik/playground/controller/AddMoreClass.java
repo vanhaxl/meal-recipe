87dd6247be59e3e73f198c2028497350e5be5705
Van Ha Nguyen <vanhanguyen@Vans-MacBook-Pro.local>
2018-09-19 08:49:17 -0500

    test

--- src/main/java/altrimetrik/playground/controller/AddMoreClass.java
+++ src/main/java/altrimetrik/playground/controller/AddMoreClass.java
@@ -2,6 +2,7 @@ package altrimetrik.playground.controller;
 
 public class AddMoreClass {
     //test once more time
-    //test more
-    //test more
+    public void test(){
+        System.out.println("hi");
+    }
 }
